0d9a7b6549bc4e53c3a2a05a61f271a7b5e658cc
adamwsyaputra <adamwsyaputra@gmail.com>
2026-09-19 02:03:55 +0700

    feat(form): create SkillForm model form with specialized widgets

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
-from django.forms import ModelForm, TextInput, Textarea, URLInput
-from main.models import Project
+from django.forms import ModelForm, TextInput, Textarea, URLInput, Select, NumberInput, CheckboxInput
+from main.models import Project, Skill
 
 class ProjectForm(ModelForm):
     class Meta:
@@ -25,3 +25,46 @@ class ProjectForm(ModelForm):
             "project_url": URLInput(attrs={"placeholder": "https://github.com/username/project"}),
             "project_image_url": URLInput(attrs={"placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000"}),
         }
+
+class SkillForm(ModelForm):
+    class Meta:
+        model = Skill
+        fields = [
+            "name",
+            "category",
+            "proficiency_percent",
+            "is_core",
+        ]
+        labels = {
+            "name": "Skill Name",
+            "category": "Category",
+            "proficiency_percent": "Proficiency (%)",
+            "is_core": "Highlight as Core Skill?",
+        }
+        widgets = {
+            "name": TextInput(
+                attrs={
+                    "placeholder": "e.g., Python, Linux Kernel, Reverse Engineering",
+                    "class": "form-input",
+                    "maxlength": 100,
+                }
+            ),
+            "category": Select(
+                attrs={
+                    "class": "form-input",
+                }
+            ),
+            "proficiency_percent": NumberInput(
+                attrs={
+                    "min": 0,
+                    "max": 100,
+                    "class": "form-input",
+                    "placeholder": "85",
+                }
+            ),
+            "is_core": CheckboxInput(
+                attrs={
+                    "class": "form-checkbox",
+                }
+            ),
+    }
