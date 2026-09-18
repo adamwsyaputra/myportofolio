09f8c3bd0b8e8ba4d94d3272464e2546c5ed678f
adamwsyaputra <adamwsyaputra@gmail.com>
2026-09-19 02:36:44 +0700

    feat(url): Register URLs to main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,11 @@ from main.views import (
     show_experience,
     show_main,
     show_projects,
+    create_skill,
+    delete_skill,
+    get_skills_json,
+    show_skills,
+    update_skill,
 )
 
 app_name = "main"
@@ -17,4 +22,9 @@ urlpatterns = [
     path("projects/add/", create_project, name="create_project"),
     path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
+    path("skills/", show_skills, name="show_skills"),
+    path("skills/add/", create_skill, name="create_skill"),
+    path("skills/<uuid:skill_id>/edit/", update_skill, name="update_skill"),
+    path("skills/<uuid:skill_id>/delete/", delete_skill, name="delete_skill"),
+    path("api/skills/", get_skills_json, name="get_skills_json"),
 ]
